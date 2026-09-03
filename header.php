2059ead7b6b700f5646b0be51c80e832d1dfb3df
Kyle Christian Lopez Casipit <kylechristianlcasipit@iskolarngbayan.pup.edu.ph>
2026-09-03 20:02:59 +0800

    Add SEO meta tags, title tags, OpenGraph data & font preconnect across all pages

--- header.php
+++ header.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
